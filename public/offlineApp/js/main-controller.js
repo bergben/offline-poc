6a4ce9784e7cac6a41eb41830039dca66d740b0b
Benedikt Berger <benedikt.berger@live.de>
2016-05-17 11:59:54 +0200

    sync

--- public/offlineApp/js/main-controller.js
+++ public/offlineApp/js/main-controller.js
@@ -8,10 +8,11 @@
         var self = this;
         self.items=[];
         $localForage.iterate(function(item) {
+          console.log(item);
           if(item.key!="synced")
             self.items.push(item);
         }).then(function(data) {
-          console.log(data);
+          console.log(self.items);
         });
       }
     ]);
