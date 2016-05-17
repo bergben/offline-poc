136e3ad6f34b91a7123329dc43777da3e820834a
Benedikt Berger <benedikt.berger@live.de>
2016-05-17 13:52:29 +0200

    sync

--- public/offlineApp/js/main-controller.js
+++ public/offlineApp/js/main-controller.js
@@ -7,12 +7,36 @@
       function($localForage) {
         var self = this;
         self.items=[];
+        self.images=[];
         $localForage.iterate(function(item) {
           if(item.name!=null)
             self.items.push(item);
+          if(item.size!=null){
+            if(!reader)
+            var reader = new window.FileReader();
+            
+            reader.readAsDataURL(item); 
+            reader.onloadend = function() {
+                              base64data = reader.result;    
+                              self.images.push(base64data);  
+            }
+          }
         }).then(function(data) {
           console.log(self.items);
         });
+        
+        self.getDBSize=function(){
+            self.totalDBSize=0;
+            $localForage
+                .iterate(function(item) {
+                        if(item.size!=null){
+                            self.totalDBSize+=item.size;
+                        }
+                    }
+                 ).then(function(data) {
+                 })
+            ;
+        }
       }
     ]);
 }(window.angular));
